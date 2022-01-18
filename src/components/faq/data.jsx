67059786bf3039ee1b796428f5de87cd5dcff556
AgustinTafura <agustintafura@hotmail.com>
2022-01-18 15:56:39 -0300

    faqs

---
+++ src/components/faq/data.jsx
@@ -0,0 +1,63 @@
+export const data = [
+    {
+        q: '¿Cómo limpiar tus Alienadores Invisibles?',
+        r: [
+            'Es fundamental el cepillado diario tus alineadores SMILE MAKERS, lo ideal sería dos veces al día. Además recomendamos utilizaar las pastillas efervescentes de corega, sumergiéndolos de 2 a 3 minutos diariamente o día por medio.',
+            'Los alineadores pueden mancharse pues absorben la pigmentación de las comidas y bebidas que consumimos a diario. Cuánto se manchen va a depender de los hábitos alimenticios de cada persona. Bebidas como vino, café, mate, tienden a mancharlos, así como también el cigarrillo.',
+            'Sin embargo, la utilización de la placas protegerán el esmalte de tus dientes de la acción nociva de éstos productos. Por este motivo es tan importante que higienices tus alineadores SMILE MAKERS a diario y con los productos recomendados.',
+            'De esta forma tus alineadores se van a ver siempre limpios y translucidos.'
+        ]
+    },
+    {
+        q: '¿Qué ocurre si se rompre alguno de mis Alineadores?',
+        r: [
+            'Como en todo tratamiento es importante seguir las recomendaciones del ortodoncista debido a que las placas podrían romperse por mal uso.',
+            'En estos casos comunicate de inmediato con nosotros para que hagamos la reposicion rápidamente.'
+        ]
+    },
+    {
+        q: '¿Es un tratamiento doloroso?',
+        r: [
+            'Los alineadores invisibles SMILE MAKERS no modifican el habla, puede ser que el primer día de uso sientas alguna diferencia pero pasado este tiempo no vas a sentir que los tenes puestos.',
+            'Al ser un tratamiento donde hay un movimiento progresivo de tus dientes, puede ser que sientas un leve dolor cada vez que cambiemos tus placas, que se va a disipar cuando estos se adapten al nuevo alineador.',
+        ]
+    },
+    {
+        q: '¿Cuánto tiempo debo cada Alineador?',
+        r: [
+            'Son super cómodos, están confeccionados con materiales flexibles y de buena calidad.',
+            'Podes usarlos todo el día, solo deberas sacártelos para comer o cepillarte los dientes.'
+        ]
+    },
+    {
+        q: '¿Qué incluye el Kit Inicial?',
+        r: [
+            'Con la entrega de tus primeros alineadores SMILE MAKERS vamos a entregarte:',
+            '- Un neceser',
+            '- Pastillas blanqueadoras Corega para que puedas limpiarlos,',
+            '- Un cepillo de dientes @merakisustentable que son un 💯,',
+            '- Estuche para que puedas guardarlos cuando no los estes usando,',
+            '- Pasta dental,',
+            '- Elemento para quitar tus placas con mayor facilidad,',
+            '- Alcohol en aerosol para que lo lleves a todos lados y siempre que te los quites estes seguro de que tenes las manos desinfectadas.',
+        ]
+    },
+    {
+        q: '¿Cada cuánto tiempo debo cambiar los Alineadores?',
+        r: [
+            'Los alineadores invisibles SMILE MAKERS se cambian cada 20 dias. No es necesario que el cambio se realice siempre en consultorio.',
+            'Dependiendo del caso, podemos entregarte algunas placas para que vos lo hagas en tu casa en la fecha pactada con el ortodoncista. Esto es una gran ventaja ya que podes irte de viaje y no retrasar el tiempo del tratamiento.'
+        ]
+    },
+    {
+        q: '¿Quienes pueden utilizar Alineadores Invisibles?',
+        r: [
+            'Hoy en día el 90 % de los problemas de alineacion dentaria pueden ser resueltos mediante ortodoncia invisible. Lps alineadores realizan fuerzas digitalmente calculadas y de baja intensidad ideales para corregir cualquier tipo de caso.',
+            'Existen algunos casos en los cuales no se puede utilizar ortodoncia invisible:',
+            '* Pacientes con problemas periodontales, los cuales deberán tratar esta afección para poder utilizarlos.',
+            '* Pacientes con perdida ósea.',
+            '* Pacientes con reabsorciones de raíces.',
+            'Siempre es ideal tener una consulta con el ortodoncista, ya que muchos de los problemas por los cuales no se pueden utilizar alineadores son modificables.'
+        ]
+    }
+]
