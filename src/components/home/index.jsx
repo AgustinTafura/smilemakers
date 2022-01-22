86bd349b752598f1f5e7cffdeef23135a924ff57
AgustinTafura <agustintafura@hotmail.com>
2022-01-22 00:39:28 -0300

    home mobile responsive

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import './index.scss'
+
 export const Home = () => {
 
     const [windowWidth, setWindowWidth] = useState()
     const imageSmall = `${process.env.PUBLIC_URL}/images/homeSmall.jpg`;
     const imageBig = `${process.env.PUBLIC_URL}/images/homeBig.jpg`;
 
-   
-
     useEffect(() => {
-
         setWindowWidth(window.innerWidth)
-
-        window.addEventListener('resize', () => {
-            setWindowWidth(window.innerWidth)
-          })        
-    }, [])
+        window.addEventListener('resize', ()=>setWindowWidth(window.innerWidth))
+        
+        return () => {
+            window.removeEventListener('resize',()=>setWindowWidth(window.innerWidth))
+        }
+    }, []) 
 
     return (
         <>
@@ -30,16 +30,22 @@ export const Home = () => {
             </div>
 
 
-            <div className="site-blocks-cover" style={{backgroundImage: `url(${windowWidth < 576 ? imageSmall : imageBig })`}} data-aos="fade"
+            <div id='home' className="site-blocks-cover" style={{backgroundImage: `url(${windowWidth < 576 ? imageSmall : imageBig })`}} data-aos="fade"
                 data-stellar-background-ratio="0.5">
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-md-6">
-                            <span className="sub-text fw-bolder">ALINEADORES INVISIBLES</span>
-                            <h1>QUEREMOS <strong>VERTE SONREIR</strong></h1>
-                        <Link to='#alineadores'>
-                            <button className="btn btn-outline-light my-3">Quiero mis SM®</button>
-                        </Link>
+                        <div className="col-md-6 title-container">
+                            <div className="title-body">
+                                <span className="sub-text fw-bolder">ALINEADORES INVISIBLES</span>
+                                <h1>
+                                    QUEREMOS
+                                    <br/>
+                                    <strong>VERTE
+                                    SONREIR</strong></h1>
+                            </div>
+                            <Link to='#alineadores' className='title-button'>
+                                <button className="btn btn-outline-light my-3">Quiero mis SM®</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
